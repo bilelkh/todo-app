62cf944ddcb115e37abc888068f21b7094424950
bilelk <bilel1996k@gmail.com>
2020-04-09 20:53:57 +0100

    add api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +17,8 @@ import { PostesService } from './componentes/services/postes.service'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [PostesService],
   bootstrap: [AppComponent]
--- src/app/componentes/post-list/post-list.component.ts
+++ src/app/componentes/post-list/post-list.component.ts
@@ -6,17 +6,31 @@ import { PostesService } from '../services/postes.service'
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-
+  postes: any[] = [];
   constructor(public postesService: PostesService) {
     console.log('===constructor ===')
   }
 
   ngOnInit() {
-    this.getPostes()
-
+    this.getPostes();
   }
 
   getPostes() {
+
+    // console.log("===getPostes===")
+    // this.postesService.getPostes().subscribe(data => {
+    //   console.log("===datat===", data);
+    // }, error => {
+    //   console.error("===error===", error);
+    // })
+
+    this.postesService.getPostes().toPromise().then((data:any) => {
+      this.postes = data;
+      console.log("===data===", data);
+    }).catch(error => {
+      console.log("==error==", error);
+    })
+
 
   }
 
